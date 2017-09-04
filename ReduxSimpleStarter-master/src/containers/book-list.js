b3672c9efaa608f3dcec165ee9f670d88e266e52
fatcatdog <duchenjacob@gmail.com>
2017-09-04 13:50:11 -0400

    Finish project

---
+++ ReduxSimpleStarter-master/src/containers/book-list.js
@@ -0,0 +1,40 @@
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { selectBook } from '../actions/index';
+import { bindActionCreators } from 'redux';
+
+class BookList extends Component {
+  renderList() {
+    return this.props.books.map((book) => {
+      return (
+        <li
+          key={book.title}
+          onClick={() => this.props.selectBook(book)}
+          className="list-group-item">{book.title}
+        </li>
+      );
+    });
+  }
+
+  render () {
+    return (
+      <ul className="list-group col-sm-4">
+      {this.renderList()}
+      </ul>
+    )
+  }
+}
+
+function mapStateToProps(state) {
+  //whatever is returned will show up as props inside of BookList
+  return {
+    books: state.books
+  };
+}
+//anything returned from this function will end up as props on the BookList continaer
+function mapDispatchToProps(dispatch) {
+  //whenever selectBook is called, the result should be passed to all of our reducers (which is what bindactioncreators is doing)
+  return bindActionCreators({ selectBook: selectBook }, dispatch);
+}
+//promote booklist from a component to a container
+export default connect(mapStateToProps, mapDispatchToProps)(BookList);
---
+++ ReduxSimpleStarter-master/src/reducers/reducer_books.js
@@ -0,0 +1,8 @@
+export default function() {
+  return [
+    { title: 'Javascript', pages: 101 },
+    { title: 'Harry Potter', pages: 102 },
+    { title: 'Lord of the Rings', pages: 104 },
+    { title: 'Ruby is king', pages: 106 }
+  ]
+}
